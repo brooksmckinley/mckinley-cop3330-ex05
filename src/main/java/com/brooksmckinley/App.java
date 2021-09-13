8c5313dabe0182dcc54c38ffc1896fe26372078f
Brooks McKinley <brooksmckinley@yahoo.com>
2021-09-13 18:00:56 -0400

    Initial commit

---
+++ src/main/java/com/brooksmckinley/App.java
@@ -0,0 +1,29 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Brooks McKinley
+ */
+
+package com.brooksmckinley;
+
+public class App {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static int divide(int a, int b) {
+        return a / b;
+    }
+}
---
+++ src/test/java/com/brooksmckinley/AppTest.java
@@ -0,0 +1,42 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Brooks McKinley
+ */
+
+package com.brooksmckinley;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    public void testAdd() {
+        Assertions.assertEquals(App.add(1, 1), 2);
+        Assertions.assertEquals(App.add(2, 2), 4);
+        Assertions.assertEquals(App.add(123, 234), 357);
+    }
+
+    @Test
+    public void testSubtract() {
+        Assertions.assertEquals(App.subtract(1, 1), 0);
+        Assertions.assertEquals(App.subtract(123, 23), 100);
+        Assertions.assertEquals(App.subtract(100, 123), -23);
+    }
+
+    @Test
+    public void testMultiply() {
+        Assertions.assertEquals(App.multiply(3, 3), 9);
+        Assertions.assertEquals(App.multiply(123, -1), -123);
+        Assertions.assertEquals(App.multiply(10, 9), 90);
+        Assertions.assertEquals(App.multiply(21839283, 1), 21839283);
+    }
+
+    @Test
+    public void testDivide() {
+        Assertions.assertEquals(App.divide(10, 2), 5);
+        Assertions.assertEquals(App.divide(5, 2), 2);
+        Assertions.assertEquals(App.divide(4, 1), 4);
+        Assertions.assertEquals(App.divide(4, -1), -4);
+    }
+}
